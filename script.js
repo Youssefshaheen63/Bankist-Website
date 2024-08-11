4214415219f9d8be2eeb41201760d8af89f0066d
Youssef Shaheen <shaheeny616@gmail.com>
2024-08-11 20:10:40 +0300

    Reveal Sections

--- script.js
+++ script.js
@@ -164,3 +164,25 @@ const headerobserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerobserver.observe(header);
+
+// Reveal Sections
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (sec) {
+  sectionObserver.observe(sec);
+  sec.classList.add('section--hidden');
+});
